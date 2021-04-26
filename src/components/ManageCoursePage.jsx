1f0ac7b360061fc354fd25e8ec3b1615b579979c
BANI GIT <bani.rezaie@gmail.com>
2021-04-26 17:06:27 +0100

    promt
    it's not how I use it here, usually we must check for example if a state is empty and person wants to submit prompt up, or leaving a page without saving, and usually use a condition which if it's true it come up. but here I did a silly when={true}.

--- src/components/ManageCoursePage.jsx
+++ src/components/ManageCoursePage.jsx
@@ -4,6 +4,12 @@ const ManageCoursePage = props => {
   return (
     <>
       <h2>Manage Course</h2>
+      {
+        <Prompt
+          when={true}
+          message='Are you sure you want to leave this page? ha?'
+        />
+      }
       {props.match.params.slug}
     </>
   )
